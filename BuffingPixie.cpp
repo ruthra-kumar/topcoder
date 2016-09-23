0ec7dcb6c0fb7151ee807028e2ea7bee4f3e3986
ruthra kumar <ruthrab@gmail.com>
2016-09-23 17:06:17 +0530

    Create BuffingPixie.cpp

---
+++ BuffingPixie.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class   BuffingPixie
+{
+  public:
+  int fastestVictory(int HP, int normalAttack, int buffedAttack)
+  {
+      int   total_attacks   =   0;
+      bool  Focused =   0;
+      while(HP  >   0)
+      {
+          //if enemy can be defeated in single normal attack  or
+          //if previous step was focues                       or
+          //if normal attack for two consecutive steps is same as focued attack 
+          //the if block executes
+          if(normalAttack >=    HP  ||  Focused ||  (buffedAttack    <=  normalAttack*2)    )
+          {
+              
+              if(Focused)
+                HP  -=  buffedAttack;
+              else
+                HP  -=  normalAttack;
+                
+            Focused =   0;
+          }
+          else
+            Focused =   1;
+        
+        total_attacks++;
+      }
+      return    total_attacks;
+  }
+};

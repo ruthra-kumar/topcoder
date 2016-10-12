135019a7eba01e21b556a107081fbd7b37e43a78
ruthra kumar <ruthrab@gmail.com>
2016-10-12 07:05:11 +0530

    Update master.cpp

--- master.cpp
+++ master.cpp
@@ -6,12 +6,22 @@
 
 using namespace std;
 
+void    Print(vector<int>   a)
+{
+    for(auto &x:a)
+    {
+        cout    <<  x   << " ";
+    }
+}
+
 class MasterMind
 {
     public:
-        int colCount[MAX_COL];
-        int cols,len,Sum;
-        int lastCheck   =   0;
+        int     colCount[MAX_COL];
+        int     cols,len,Sum,x,y,lastCheck   =   0;
+        bool    First   =   1;
+        short   Score[MAX_LEN];
+        vector<int> prev_score;
 
         vector<int> init(int    k,int   l)
         {
@@ -19,6 +29,12 @@ class MasterMind
             {
                 colCount[i] =   0;
             }
+            for(int i=0;i<MAX_LEN;i++)
+            {
+                Score[i]    =   0;
+            }
+
+            Sum     =   0;
 
             cols    =   k;
             len     =   l;
@@ -41,44 +57,89 @@ class MasterMind
             }
             else    if(Sum  ==  len)
             {
-                vector<int> next;
-
-                for(int i=0;i<MAX_COL;i++)
+                if(First)
                 {
-                    if(colCount[i]  !=  0)
+                    //if color count found
+                    x   =   0;
+                    y   =   x+1;
+
+                    vector<int> next;
+
+                    for(int i=0;i<MAX_COL;i++)
                     {
-                        for(int j=0;    j<colCount[i];  j++)
+                        if(colCount[i]  !=  0)
                         {
-                            next.push_back(i);
+                            for(int j=0;    j<colCount[i];  j++)
+                            {
+                                next.push_back(i);
+                            }
                         }
                     }
+                    First   =   0;
+                    prev_score  =   prev;
+                    return  next;
+                }
+                else
+                {
+                    //Print score
+                    /*
+                    for(int i=0;    i<prev.size();  i++)
+                    {
+                        cout<<Score[i]  << " ";
+                    }
+                    */
+                    //Start Guessing
+                    int hit_change  =   (prev[0]    -   prev_score[0]);
+
+                    if(hit_change   ==  2   ||  hit_change  ==  -2)
+                    {
+                        Score[x]    =   Score[y]    =   2;
+                    }
+
+
+                    while(Score[y]  ==  2   &&  y   <   prev.size())  y++;
+
+
+
+                    /*
+                    y++;
+                    if( !(y    <   prev.size()) )
+                    {
+                        x++;
+                        if(!(x  <   prev.size()) )
+                        {
+                            x   =   0;
+                        }
+                        y   =   0;
+                    }
+                    */
+
+                    //swap
+                    int tmp =   prev[x];
+                    prev[x] =   prev[y];
+                    prev[y] =   tmp;
+                    cout    <<  x   <<" "   <<  y   <<'\t';
+                    return  prev;
                 }
 
-                return  next;
             }
 
             return  vector<int>(len,lastCheck++);
         }
 };
 
-void    Print(vector<int>   a)
-{
-    for(auto &x:a)
-    {
-        cout    <<  x   << " ";
-    }
-}
-
 int main()
 {
+    vector<int> code{4,3,0,1,2,5,9,7,8,6};
+
+    int l   =  code.size();
+
     MasterMind  myguess;
-    int l   =   5;
     vector<int> used(l,0);
-    vector<int> code{4,3,0,1,2};
     vector<int> results(2,0);
     vector<int> guess   =   myguess.init(l,l);
 
-    for(int i=0;i<30;i++)
+    for(int i=0;i<40;i++)
     {
         Print(guess);
 
@@ -109,7 +170,7 @@ int main()
         }
 
         cout    <<" || "    <<  results[0]  << " "  <<  results[1]  <<  '\n';
-        cin.get();
+        //cin.get();
         guess   =   myguess.nextGuess(guess,results);
     }
 
